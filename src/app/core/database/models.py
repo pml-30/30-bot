b9cd03cf62989c5bff2162fbf320b693967f0e78
dsodx <dsodx@pm.me>
2023-08-16 19:10:03 +0300

    ORM improvements

--- src/app/core/database/models.py
+++ src/app/core/database/models.py
@@ -1,7 +1,6 @@
 from datetime import datetime, date as date_type
-from typing import List
 
-from sqlalchemy.dialects.postgresql import BIGINT, TIMESTAMP, VARCHAR, INTEGER, DATE, ARRAY
+from sqlalchemy.dialects.postgresql import BIGINT, SMALLINT, TIMESTAMP, VARCHAR, INTEGER, DATE, ARRAY
 from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import Base
@@ -14,7 +13,7 @@ class UserModel(Base):
 
 
 class LessonModel(Base):
-    id: Mapped[int] = mapped_column(BIGINT(), primary_key=True)
+    id: Mapped[int] = mapped_column(SMALLINT(), primary_key=True)
     name: Mapped[str] = mapped_column(VARCHAR())
     room: Mapped[int] = mapped_column(VARCHAR())
     teacher: Mapped[str] = mapped_column(VARCHAR())
@@ -22,13 +21,13 @@ class LessonModel(Base):
 
 class DayModel(Base):
     id: Mapped[int] = mapped_column(BIGINT(), primary_key=True)
-    weekday: Mapped[int] = mapped_column(INTEGER())
     date: Mapped[date_type] = mapped_column(DATE())
-    lessons: Mapped[List[int]] = mapped_column(ARRAY(INTEGER()))
+    weekday: Mapped[int] = mapped_column(SMALLINT())
+    lessons: Mapped[list[int]] = mapped_column(ARRAY(SMALLINT()))
     location: Mapped[str] = mapped_column(VARCHAR())
 
 
 class DefaultDayModel(Base):
-    weekday: Mapped[int] = mapped_column(INTEGER(), primary_key=True)
-    lessons: Mapped[List[int]] = mapped_column(ARRAY(INTEGER()))
+    weekday: Mapped[int] = mapped_column(SMALLINT(), primary_key=True, autoincrement=False)
+    lessons: Mapped[list[int]] = mapped_column(ARRAY(INTEGER()))
     location: Mapped[str] = mapped_column(VARCHAR())
