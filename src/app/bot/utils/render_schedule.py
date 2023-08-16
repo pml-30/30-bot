963b9fed5ea331e17c65413d219961ec8f00998d
dsodx <dsodx@pm.me>
2023-08-16 19:33:31 +0300

    Refactor rendering

--- src/app/bot/utils/render_schedule.py
+++ src/app/bot/utils/render_schedule.py
@@ -1,11 +1,10 @@
 import asyncio
 import base64
 from pathlib import Path
-from typing import List
 
 from aiohttp import ClientSession
 from jinja2 import Environment, FileSystemLoader
-from src.app.core.config import ApplicationSettings
+
 from src.app.core.dto.database import LessonDTO, DefaultDayDTO, DayDTO
 
 DAYS = {
@@ -19,7 +18,7 @@ DAYS = {
 }
 
 
-async def render_svg(lessons: List[LessonDTO], schedule: DefaultDayDTO | DayDTO, templates_path: Path) -> str:
+async def render_svg(lessons: list[LessonDTO], schedule: DefaultDayDTO | DayDTO, templates_path: Path) -> str:
     env = Environment(
         loader=FileSystemLoader(templates_path),
         enable_async=True
@@ -41,9 +40,9 @@ async def render_svg(lessons: List[LessonDTO], schedule: DefaultDayDTO | DayDTO,
     return await env.get_template("schedule.svg").render_async(data)
 
 
-async def render_png(input_svg: str, config: ApplicationSettings) -> bytes:
+async def render_png(input_svg: str, api_key: str) -> bytes:
     data = {
-        "apikey": config.convertio.key.get_secret_value(),
+        "apikey": api_key,
         "input": "base64",
         "file": base64.b64encode(input_svg.encode()).decode(),
         "filename": "sh.svg",
@@ -51,25 +50,25 @@ async def render_png(input_svg: str, config: ApplicationSettings) -> bytes:
     }
 
     async with ClientSession() as session:
-        async with session.post("https://api.convertio.co/convert", json=data) as request:
-            json = await request.json()
+        async with session.post("https://api.convertio.co/convert", json=data) as response:
+            json = await response.json()
             if json["status"] == "ok":
                 conv_id = json["data"]["id"]
             else:
-                raise Exception("Converio response is bad: {}".format(json))
+                raise RuntimeError("Convertio response is bad: {}".format(json))
 
         for x in range(20):
-            async with session.get(f"http://api.convertio.co/convert/{conv_id}/status") as request:
-                json = await request.json()
+            async with session.get(f"https://api.convertio.co/convert/{conv_id}/status") as response:
+                json = await response.json()
                 if json["status"] == "ok":
                     if json["data"]["step"] == "finish":
                         download_url = json["data"]["output"]["url"]
                         break
                 else:
-                    raise Exception("Converio response is bad: {}".format(json))
+                    raise RuntimeError("Convertio response is bad: {}".format(json))
             await asyncio.sleep(1)
         else:
-            raise Exception("Conversion not ended: {}".format(json))
+            raise RuntimeError("Conversion not ended: {}".format(json))
 
-        async with session.get(download_url) as request:
-            return await request.content.read()
+        async with session.get(download_url) as response:
+            return await response.content.read()
